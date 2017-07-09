acecefac57cc205b8f610e56cb63433d8514e966
yosh <yoshtalwar@gmail.com>
2017-07-09 21:36:34 +0200

    better CLI usage, fixes #15

--- src/cli.js
+++ src/cli.js
@@ -2,26 +2,37 @@
 
 'use strict'
 
-const argv = require('minimist')(process.argv.slice(2))
-const html2pug = require('./index')
-const fs = require('fs')
-const arrify = require('arrify')
+const getStdin = require('get-stdin')
+const html2pug = require('./')
+const argv = require('yargs').argv
 
-if (argv.f) {
-  const files = arrify(argv.f)
-  const options = {
-    fragment: argv.hasOwnProperty('fragment')
+const help = [
+  '\n  Usage: html2pug [options] < [file]\n',
+  '  Options:\n',
+  `    -f, --fragment          Don't wrap output in <html>/<body> tags\n`,
+  '  Examples:\n',
+  '    # Accept input from file and write to stdout',
+  '    $ html2pug < example.html\n',
+  '    # Or write to a file',
+  '    $ html2pug < example.html > example.pug \n'
+].join('\n')
+
+async function main () {
+  const fragment = argv.fragment || argv.f
+  const needsHelp = argv.help || argv.h
+
+  const stdin = await getStdin()
+
+  if (needsHelp || !stdin) {
+    console.log(help)
+  } else {
+    try {
+      const pug = await html2pug(stdin, { fragment })
+      console.log(pug)
+    } catch (e) {
+      throw e
+    }
   }
-
-  files.forEach(file =>
-    fs.readFile(file, (err, buf) => {
-      if (err) {
-        throw new Error(err)
-      } else {
-        html2pug(buf.toString(), options)
-          .then(pug => console.log(pug))
-          .catch(err => console.log(err.stack))
-      }
-    })
-  )
 }
+
+main()
--- src/index.js
+++ src/index.js
@@ -4,19 +4,27 @@ const minify = require('html-minifier').minify
 const parse5 = require('parse5')
 const Parser = require('./parser')
 
-module.exports = async (sourceHtml, opts = {}) => {
+module.exports = async (
+  sourceHtml,
+  {
+    fragment = false,
+    caseSensitive = true,
+    removeEmptyAttributes = true,
+    collapseWhitespace = true,
+    collapseBooleanAttributes = true,
+    collapseInlineTagWhitespace = true
+  } = {}
+) => {
   const html = minify(sourceHtml, {
-    removeEmptyAttributes: true,
-    collapseWhitespace: true,
-    collapseBooleanAttributes: true,
-    collapseInlineTagWhitespace: true,
-    caseSensitive: true
+    removeEmptyAttributes,
+    collapseWhitespace,
+    collapseBooleanAttributes,
+    collapseInlineTagWhitespace,
+    caseSensitive
   })
 
   // Server-side
-  const document = opts.fragment
-    ? parse5.parseFragment(html)
-    : parse5.parse(html)
+  const document = fragment ? parse5.parseFragment(html) : parse5.parse(html)
 
   const parser = new Parser(document)
   return parser.parse()
