01fb608faf60330ce79f0b55332d26f3677fe56d
yosh <yoshtalwar@gmail.com>
2019-09-02 23:57:43 +0200

    Rewrite Parser in TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,30 @@
-const { minify } = require('html-minifier')
-const { parse, parseFragment } = require('parse5')
-const Pugify = require('./parser')
+import { minify } from 'html-minifier';
+import { DefaultTreeDocument, parse, parseFragment } from 'parse5';
+import { defaultOptions, Options } from './Options';
+import Pugify from './Parser';
 
-const defaultOptions = {
-  // html2pug options
-  fragment: false,
-  tabs: false,
-  commas: true,
-  doubleQuotes: false,
+export default (sourceHtml: string, options?: Options) => {
+  const opts: Options = options
+    ? { ...defaultOptions, ...options }
+    : defaultOptions;
 
-  // html-minifier options
-  caseSensitive: true,
-  removeEmptyAttributes: true,
-  collapseWhitespace: true,
-  collapseBooleanAttributes: true,
-  preserveLineBreaks: true,
-}
-
-module.exports = (sourceHtml, options = {}) => {
   // Minify source HTML
-  const opts = { ...defaultOptions, ...options }
-  const html = minify(sourceHtml, opts)
+  const html: string = minify(sourceHtml, opts);
 
-  const { fragment, tabs, commas, doubleQuotes } = opts
-
-  // Parse HTML and convert to Pug
-  const doc = fragment ? parseFragment(html) : parse(html)
-  const pugify = new Pugify(doc, {
+  const {
+    fragment,
     tabs,
     commas,
     doubleQuotes,
-  })
-  return pugify.parse()
-}
+  }: {
+    fragment: boolean;
+    tabs: boolean;
+    commas: boolean;
+    doubleQuotes: boolean;
+  } = opts;
+
+  // Parse HTML and convert to Pug
+  const doc = fragment ? parseFragment(html) : parse(html);
+  const pugify = new Pugify(doc as DefaultTreeDocument, opts);
+  return pugify.parse();
+};
